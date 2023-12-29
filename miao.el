e38b82e9339f45944b79bd0a193ba00cb553fa6e
Yayu Wang <st.saint.wyy@gmail.com>
2023-12-29 14:53:53 -0800

    update: define keys

--- miao.el
+++ miao.el
@@ -25,28 +25,13 @@
 
 ;;; Code:
 
-(make-variable-buffer-local
- (defvar foo-count 0
-   "Number of foos inserted into the current buffer."))
-
-(defvar miao-state-mode-alist
-  '((normal . miao-normal-mode)
-    (insert . miao-insert-mode)
-    (keypad . miao-keypad-mode))
-  "Alist of miao states -> modes")
-
-(defun insert-foo ()
-  (interactive)
-  (setq foo-count (1+ foo-count))
-  (insert "foo"))
-
 (defvar-local miao--current-state nil
   "A symbol represent current state.")
 
-(defvar-local miao--keypad-previous-state nil
+(defvar-local miao--leader-previous-state nil
   "A symbol represent current state.")
 
-(defvar miao--keypad-keys nil)
+(defvar miao--leader-keys nil)
 (defvar meow--prefix-arg nil)
 
 (defmacro miao--state-mode-p (name)
@@ -71,18 +56,22 @@
 (define-minor-mode miao-mode
   "Get your foos in the right places."
   :lighter " Miao"
-  :global t
   :keymap (let ((map (make-sparse-keymap)))
             (define-key map (kbd "C-c ESC") 'miao-normal-mode)
-	    (define-key map (kbd "<space>") 'miao-keypad-mode)
+	    (define-key map (kbd "<space>") 'miao-leader-mode)
             map)
   (if miao-mode
       (progn
-	(setq miao--current-state 'normal)
 	(miao-normal-mode t)
 	(message "enable normal mode")
 	)
     (miao--disable-current-mode)))
+
+;;;###autoload
+(define-global-minor-mode miao-global-mode miao-mode
+  (lambda ()
+    (unless (minibufferp)
+      (miao-mode 1))))
 
 (defun miao--disable-current-mode ()
   (when miao--current-state
@@ -93,24 +82,32 @@
 (defvar miao-normal-state-keymap
   (let ((keymap (make-sparse-keymap)))
     (suppress-keymap keymap t)
-    (define-key keymap (kbd "C-c f") 'insert-foo)
     (define-key keymap (kbd "i") 'miao-insert-mode)
     (define-key keymap (kbd "a") 'miao-insert-mode)
     (define-key keymap (kbd "j") 'next-line)
     (define-key keymap (kbd "k") 'previous-line)
     (define-key keymap (kbd "h") 'left-char)
     (define-key keymap (kbd "l") 'right-char)
-    (define-key keymap (kbd "<SPC>") 'miao-keypad-mode)
+    (define-key keymap (kbd "<SPC>") 'miao-leader-mode)
     keymap)
   "Keymap for Miao normal state.")
 
-(defvar miao-keypad-leader-keymap
+(defvar miao-insert-state-keymap
+  (let ((keymap (make-sparse-keymap)))
+    (define-key keymap (kbd "<escape>") 'miao-normal-mode)
+    keymap)
+  "Keymap for Miao insert state.")
+
+(defvar miao-leader-state-keymap
   (let ((keymap (make-sparse-keymap)))
     (suppress-keymap keymap t)
     (define-key keymap (kbd "b r") 'revert-buffer)
     (define-key keymap (kbd "<SPC>") 'beacon-blink)
+    (define-key keymap (kbd "<escape>") 'miao-leader-quit)
+    (define-key keymap [remap keyboard-quit] 'miao-leader-quit)
+    (define-key keymap [remap self-insert-command] 'miao-leader-self-insert)
     keymap)
-  "Keymap for Miao normal state.")
+  "Keymap for Miao leader state.")
 
 (define-minor-mode miao-normal-mode
   "Get your foos in the right places."
@@ -130,6 +127,18 @@
 
 (miao--state-mode-p normal)
 
+(defvar miao-state-mode-alist
+  '((normal . miao-normal-mode)
+    (insert . miao-insert-mode)
+    (leader . miao-leader-mode))
+  "Alist of miao states -> modes")
+
+(defvar miao-keymap-alist
+  `((normal . ,miao-normal-state-keymap)
+    (insert . ,miao-insert-state-keymap)
+    (leader . ,miao-leader-state-keymap))
+  "Alist of symbols of state names to keymaps.")
+
 (defun miao-append ()
   (interactive)
   (miao-insert-mode t)
@@ -139,9 +148,7 @@
 (define-minor-mode miao-insert-mode
   "Get your foos in the right places."
   :lighter " /ᐠ.ꞈ.ᐟ\\"
-  :keymap (let ((map (make-sparse-keymap)))
-            (define-key map (kbd "<escape>") 'miao-normal-mode)
-            map)
+  :keymap miao-insert-state-keymap
   (message "before insert %s" miao--current-state)
   (if miao-insert-mode
       (if (not (equal miao--current-state 'insert))
@@ -157,34 +164,29 @@
 (miao--state-mode-p insert)
 
 
-(define-minor-mode miao-keypad-mode
+(define-minor-mode miao-leader-mode
   "Get your foos in the right places."
   :lighter " ಎ-ω-ಎ"
-  :keymap (let ((map (make-sparse-keymap)))
-	    (suppress-keymap map t)
-	    (define-key map (kbd "<escape>") 'miao-keypad-quit)
-	    (define-key map [remap keyboard-quit] 'miao-keypad-quit)
-	    (define-key map [remap self-insert-command] 'miao-keypad-self-insert)
-            map)
-  (message "before keypad %s %s" miao-keypad-mode miao--current-state)
-  (setq miao--keypad-previous-state miao--current-state)
-  (if miao-keypad-mode
-      (if (not (equal miao--current-state 'keypad))
-	  ;; switch to keypad mode: disable current + set keypad
+  :keymap miao-leader-state-keymap
+  (message "before leader %s %s" miao-leader-mode miao--current-state)
+  (setq miao--leader-previous-state miao--current-state)
+  (if miao-leader-mode
+      (if (not (equal miao--current-state 'leader))
+	  ;; switch to leader mode: disable current + set leader
 	  (progn
 	    (miao--disable-current-mode)
-	    (setq miao--current-state 'keypad)))
+	    (setq miao--current-state 'leader)))
     (setq miao--current-state nil)
     )
-  (message "after keypad %s" miao--current-state))
+  (message "after leader %s" miao--current-state))
 
-(defun miao-keypad-quit ()
-  "miao keypad state quit and switch to previous state"
+(defun miao-leader-quit ()
+  "miao leader state quit and switch to previous state"
   (interactive)
   (miao-switch-to-previous-state)
-  (message "keypad to %s" miao--keypad-previous-state)
-  (setq miao--keypad-previous-state nil)
-  (setq miao--keypad-keys nil))
+  (message "leader to %s" miao--leader-previous-state)
+  (setq miao--leader-previous-state nil)
+  (setq miao--leader-keys nil))
 
 (defun miao--switch-state (state)
   "Switch to STATE execute 'miao-switch-state-hook unless NO-HOOK is non-nil."
@@ -196,7 +198,7 @@
   miao--current-state)
 
 (defun miao-switch-to-previous-state ()
-  (miao--switch-state miao--keypad-previous-state))
+  (miao--switch-state miao--leader-previous-state))
 
 (defun miao--event-key (event)
   (let ((c (event-basic-type event)))
@@ -224,58 +226,58 @@
    (t nil)))
 
 
-(defun miao-keypad-self-insert ()
-  "Default command when keypad state is enabled."
+(defun miao-leader-self-insert ()
+  "Default command when leader state is enabled."
   (interactive)
   (setq this-command last-command)
   (when-let ((event (miao--event-key last-input-event))
              (key (miao--parse-input-event event)))
-    (push (cons 'literal key) miao--keypad-keys)
+    (push (cons 'literal key) miao--leader-keys)
     ;; Try execute if the input is valid.
-    (miao--keypad-try-execute)))
+    (miao--leader-try-execute)))
 
-(defun miao--keypad-lookup-key (keys)
-  (let* ((overriding-local-map miao-keypad-leader-keymap)
+(defun miao--leader-lookup-key (keys)
+  (let* ((overriding-local-map miao-leader-state-keymap)
          (keybind (key-binding keys)))
       keybind))
 
-(defun miao--keypad-try-execute ()
+(defun miao--leader-try-execute ()
   "Try execute command.
 
 If there is a command available on the current key binding,
 try replacing the last modifier and try again."
-  (let* ((key-str (miao--keypad-format-keys nil))
-         (cmd (miao--keypad-lookup-key (read-kbd-macro key-str))))
-    (message "keypad-keys %s key-str: %s cmd: %s" miao--keypad-keys key-str cmd)
+  (let* ((key-str (miao--leader-format-keys nil))
+         (cmd (miao--leader-lookup-key (read-kbd-macro key-str))))
+    (message "leader-keys %s key-str: %s cmd: %s" miao--leader-keys key-str cmd)
     (cond
      ((commandp cmd t)
       (setq current-prefix-arg miao--prefix-arg
             miao--prefix-arg nil)
-      (let ((miao--keypad-this-command cmd))
-        (miao-keypad-quit)
+      (let ((miao--leader-this-command cmd))
+        (miao-leader-quit)
         (setq real-this-command cmd
               this-command cmd)
         (call-interactively cmd)))
      ((keymapp cmd)
       t)
-     ((equal 'control (caar miao--keypad-keys))
-      (setcar miao--keypad-keys (cons 'literal (cdar miao--keypad-keys)))
-      (miao--keypad-try-execute))
+     ((equal 'control (caar miao--leader-keys))
+      (setcar miao--leader-keys (cons 'literal (cdar miao--leader-keys)))
+      (miao--leader-try-execute))
      (t
       (setq miao--prefix-arg nil)
-      (message "%s is undefined" (miao--keypad-format-keys nil))
-      (miao-keypad-quit)))))
+      (message "%s is undefined" (miao--leader-format-keys nil))
+      (miao-leader-quit)))))
 
-(defun miao--keypad-format-single-key (key)
+(defun miao--leader-format-single-key (key)
   "Return a display format for input KEY."
   (message "single-key %s" key)
   (cl-case (car key)
     (meta (format "M-%s" (cdr key)))
-    (control (format "C-%s" (miao--keypad-format-upcase (cdr key))))
-    (both (format "C-M-%s" (miao--keypad-format-upcase (cdr key))))
+    (control (format "C-%s" (miao--leader-format-upcase (cdr key))))
+    (both (format "C-M-%s" (miao--leader-format-upcase (cdr key))))
     (literal (cdr key))))
 
-(defun miao--keypad-format-upcase (k)
+(defun miao--leader-format-upcase (k)
   "Return S-k for upcase k."
   (let ((case-fold-search nil))
     (if (and (stringp k)
@@ -283,12 +285,12 @@ try replacing the last modifier and try again."
         (format "S-%s" (downcase k))
       k)))
 
-(defun miao--keypad-format-keys (&optional prompt)
+(defun miao--leader-format-keys (&optional prompt)
   "Return a display format for current input keys."
   (let ((result ""))
     (setq result
           (thread-first
-              (mapcar #'miao--keypad-format-single-key miao--keypad-keys)
+              (mapcar #'miao--leader-format-single-key miao--leader-keys)
             (reverse)
             (string-join " ")))
     (cond
@@ -308,5 +310,29 @@ try replacing the last modifier and try again."
      (prompt
       (setq result (concat result " C-"))))
     result))
+
+(defun miao-define-keys (state &rest keybinds)
+  "Define KEYBINDS in STATE.
+
+Example usage:
+  (miao-define-keys
+    ;; state
+    'normal
+
+    ;; bind to a command
+    '(\"a\" . miao-append)
+
+    ;; bind to a keymap
+    (cons \"x\" ctl-x-map)
+
+    ;; bind to a keybinding which holds a keymap
+    '(\"c\" . \"C-c\")
+
+    ;; bind to a keybinding which holds a command
+    '(\"q\" . \"C-x C-q\"))"
+  (declare (indent 1))
+  (let ((map (alist-get state miao-keymap-alist)))
+    (pcase-dolist (`(,key . ,def) keybinds)
+      (define-key map (kbd key) def))))
 
 (provide 'miao)
